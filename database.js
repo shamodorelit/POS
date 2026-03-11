d6af91c6a1b5869848f00e27bb8f918a2d466efe
shamodorelit <shamodorelit@gmail.com>
2026-03-11 21:01:15 +0530

    Add files via upload

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { encrypt, decrypt } = require('./utils/encryption');
 
 // Global variable to cache the mongoose connection
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { encrypt } = require('./utils/encryption');
 const { connectDB, initializeDatabase, User, Product, Invoice } = require('./database');
 
